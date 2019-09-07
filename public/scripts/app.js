aaf5948d9ec9bc9d437fd80b0dfa7c9fb90fc5ba
Labeeb Hasan Zaker <labeebzaker@gmail.com>
2019-09-06 20:54:25 -0700

    Commit made

---
+++ public/scripts/app.js
@@ -0,0 +1,45 @@
+var className = ["html","bootstrap4", "css","js","mongodb","python","nodejs"]
+var animateHTML = function() {
+    var elems;
+    var windowHeight;
+    function init() {
+      elems = document.querySelectorAll('.skills');
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+      for (var i = 0; i < elems.length; i++) {
+        var positionFromTop = elems[i].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= 0) {
+          elems[i].className = elems[i].className.replace(
+            'zero',
+            className[i]
+          );
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+  const cursor = document.querySelector('.cursor');
+
+  document.addEventListener('mousemove', e => {
+      cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+  })
+
+  document.addEventListener('click', () => {
+      cursor.classList.add("expand");
+
+      setTimeout(() => {
+          cursor.classList.remove("expand");
+      }, 500)
+  })
+  animateHTML().init();
+
+ 
