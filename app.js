aa33032a2ef1a5460be69b01b668a8b55eb9b9c7
Labeeb Zaker <zakerl@mcmaster.ca>
2023-02-23 20:24:08 -0500

    slight modification

--- app.js
+++ app.js
@@ -61,10 +61,10 @@ app.get('/favicon.ico', function (req, res) {
   res.status(204)
 });
 
-app.listen(process.env.PORT, process.env.IP, function () {
-  console.log("server starts")
-})
-
-// app.listen(3000, function () {
+// app.listen(process.env.PORT, process.env.IP, function () {
 //   console.log("server starts")
 // })
+
+app.listen(3000, function () {
+  console.log("server starts")
+})
